65bf9e4c6ebb677926ac563a6a6c715d6a79b82f
Shaveel <shaveel.prasad17@gmail.com>
2023-03-07 16:20:25 +1100

    function to play a round and decide winner

--- rps.js
+++ rps.js
@@ -1,8 +1,54 @@
-let options = ["rock", "paper", "scissors"];
+const options = ["rock", "paper", "scissors"];
+
+
 compChoice = getComputerChoice(options)
+
+
+
+
+
 
 function getComputerChoice(options) {
     return options[Math.floor(Math.random() * 3)];
+}
+
+function playRound(comp, player) {
+    
+    if (comp === "rock") {
+        if (player === "scissors") {
+            return "You LOSE, Rock crushes Scissors!"
+        }
+        else if (player === "paper") {
+            return "You WIN!, Paper defeats Rock"
+        }
+    }
+    else if (comp === "paper") {
+        if (player === "scissors") {
+            return "You WIN!, Scissors cuts up Papa!"
+        }
+        else if (player === "rock") {
+            return "You Lose, Paper defeats Rock"
+        }
+    }
+    else if (comp === "scissors") {
+        if (player === "rock") {
+            return "You WIN!, Rock crushes Scissors!"
+        }
+        else if (player === "paper") {
+            return "You Lose, Scissors cuts up Papa!"
+        }
+    }
+    else {
+        return "Draw!"
+    }
+    
+    && player === "scissors") {
+        return "Computer WINS!"
+    }
+    else if (comp === "scissors" && player === "paper") {
+        return "Computer WINS!"
+    }
+    
 }
 
 console.log(compChoice);
