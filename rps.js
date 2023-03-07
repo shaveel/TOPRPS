02936358b1d69033ea4379b5a6f462debc223704
Shaveel <shaveel.prasad17@gmail.com>
2023-03-07 17:03:35 +1100

    function to run a 5 round game. display round by round progress

--- rps.js
+++ rps.js
@@ -2,8 +2,9 @@ const options = ["rock", "paper", "scissors"];
 
 let playerChoice = prompt("Present your weapon of choice: ");
 
+
 compChoice = getComputerChoice(options)
-roundResult = playRound(compChoice, text.toLowerCase(playerChoice))
+
 
 
 
@@ -16,30 +17,46 @@ function getComputerChoice(options) {
 function playRound(comp, player) { 
     if (comp === "rock") {
         if (player === "scissors") {
-            return "You LOSE, Rock crushes Scissors!"
+            return ["You LOSE, Rock crushes Scissors!", 0]
         }
         else if (player === "paper") {
-            return "You WIN!, Paper defeats Rock"
+            return ["You WIN!, Paper defeats Rock", 1]
         }
     }
     else if (comp === "paper") {
         if (player === "scissors") {
-            return "You WIN!, Scissors cuts up Papa!"
+            return ["You WIN!, Scissors cuts up Papa!", 1]
         }
         else if (player === "rock") {
-            return "You Lose, Paper defeats Rock"
+            return ["You Lose, Paper defeats Rock", 0]
         }
     }
     else if (comp === "scissors") {
         if (player === "rock") {
-            return "You WIN!, Rock crushes Scissors!"
+            return ["You WIN!, Rock crushes Scissors!", 1]
         }
         else if (player === "paper") {
-            return "You Lose, Scissors cuts up Papa!"
+            return ["You Lose, Scissors cuts up Papa!", 0]
         }
     }
+    else if {
+        return ["Draw!", 2]
+    }
     else {
-        return "Draw!"
+        return "Weapon is not valid for this war, please select between rock, paper or sicssors"
+    }
+}
+
+function game(compChoice, playerChoice) {
+    let score = [0, 0];
+    roundResult = playRound(compChoice, playerChoice.toLowerCase())
+    if (roundResult) {
+        score[roundResult] =+ 1;
+        console.log(`You WIN!! ${playerChoice} defeats ${compChoice}\nThe score is now   COMPUTER${score[0]}:${score[1]}YOU`);
+
+    }
+    else if (!roundResult) {
+        score[roundResult] =+ 1;
     }
 }
 
