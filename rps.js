85b7ea629bce056b1a5bcb7480555d8f9a85f533
Shaveel <shaveel.prasad17@gmail.com>
2023-03-09 11:14:41 +1100

    push bug

--- rps.js
+++ rps.js
@@ -1,63 +1,70 @@
 const options = ["rock", "paper", "scissors"];
 
-let playerChoice = prompt("Present your weapon of choice: ");
 
 
-compChoice = getComputerChoice(options)
-
-
-
-
-
+game()
 
 function getComputerChoice(options) {
     return options[Math.floor(Math.random() * 3)];
+    
 }
 
 function playRound(comp, player) { 
-    if (comp === "rock") {
+    if (comp === player){
+        return "2"
+    }
+    else if (comp === "rock") {
         if (player === "scissors") {
-            return ["You LOSE, Rock crushes Scissors!", 0]
+            return 0
         }
         else if (player === "paper") {
-            return ["You WIN!, Paper defeats Rock", 1]
+            return 1
         }
     }
     else if (comp === "paper") {
         if (player === "scissors") {
-            return ["You WIN!, Scissors cuts up Papa!", 1]
+            return 1
         }
         else if (player === "rock") {
-            return ["You Lose, Paper defeats Rock", 0]
+            return 0
         }
     }
     else if (comp === "scissors") {
         if (player === "rock") {
-            return ["You WIN!, Rock crushes Scissors!", 1]
+            return 1
         }
         else if (player === "paper") {
-            return ["You Lose, Scissors cuts up Papa!", 0]
+            return 0
         }
     }
-    else if {
-        return ["Draw!", 2]
-    }
+    
     else {
         return "Weapon is not valid for this war, please select between rock, paper or sicssors"
     }
 }
 
-function game(compChoice, playerChoice) {
+function game() {
     let score = [0, 0];
-    roundResult = playRound(compChoice, playerChoice.toLowerCase())
-    if (roundResult) {
-        score[roundResult] =+ 1;
-        console.log(`You WIN!! ${playerChoice} defeats ${compChoice}\nThe score is now   COMPUTER${score[0]}:${score[1]}YOU`);
+    let playerChoice;
+    for (i = 0; i < 5; i++) {
+        playerChoice = prompt("Present your weapon of choice: ");
+        compChoice = getComputerChoice(options)
+        roundResult = playRound(compChoice, playerChoice.toLowerCase())
+        if (roundResult === "2") {
+            console.log(`Draw! \nThe score stays at   COMPUTER ${score[0]}:${score[1]} YOU`);
+        }
+        else if (roundResult) {
+            score[roundResult] =+ 1;
+            console.log(`You WIN!! ${playerChoice} defeats ${compChoice}\nThe score after round ${i+1}   COMPUTER ${score[0]}:${score[1]} YOU`);
 
-    }
-    else if (!roundResult) {
-        score[roundResult] =+ 1;
+        }
+        else if (!roundResult) {
+            score[roundResult] =+ 1;
+            console.log(`You Lose. ${compChoice} defeats ${playerChoice}\nThe score after round ${i+1}   COMPUTER ${score[0]}:${score[1]} YOU`);
+        }
+        else {
+            console.log(roundResult);
+        }
     }
 }
 
-console.log(compChoice);
